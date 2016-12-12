55d011f1a02ad3cc91d58ed050bf9f37932c8c67
Amit Saini <asaini@miraclesoft.com>
2016-12-12 09:42:31 -0500

    Sending Changes in App

--- src/main/java/com/continuous/integration/App.java
+++ src/main/java/com/continuous/integration/App.java
@@ -2,8 +2,8 @@ package com.continuous.integration;
 
 public class App {
 
-	public String conCat(String a, String c){
-		return a+c;
+	public String conCat(String a, String cc){
+		return a+cc;
 	}
 	
 	public int add (int a, int b){
